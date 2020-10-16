8cb116cbb90f20ad2a8c219ee94333fac8a0ccd9
Giri Madhan <gmpgiri@gmail.com>
2020-10-16 11:08:25 +0530

    sidbar icons added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNw067L8gvio59eMCMGpxDVsLGibYnNzU",
+  authDomain: "whatsapp-gmp.firebaseapp.com",
+  databaseURL: "https://whatsapp-gmp.firebaseio.com",
+  projectId: "whatsapp-gmp",
+  storageBucket: "whatsapp-gmp.appspot.com",
+  messagingSenderId: "699533857851",
+  appId: "1:699533857851:web:89170854cfa77e4526781e",
+};
